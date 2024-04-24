49d20df60d521f84360e095e1f694a4fd05d05e2
Banks Troutman <btrout.dhrs@gmail.com>
2024-04-24 12:19:57 -0400

    external deps work fr this time i swear

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-
 #include <wpigui.h>
-#include <wpigui_openurl.h>
-
-#include "glass/Context.h"
-#include "glass/MainMenuBar.h"
-#include "glass/Model.h"
-#include "glass/Storage.h"
-#include "glass/View.h"
-#include "glass/other/Log.h"
-#include "glass/other/Plot.h"
 
 #include "Dbc.h"
+#include "glass/Context.h"
+#include "glass/MainMenuBar.h"
 
 namespace gui = wpi::gui;
 
@@ -26,68 +17,72 @@ int __stdcall WinMain(void* hInstance, void* hPrevInstance, char* pCmdLine,
   int argc = __argc;
   char** argv = __argv;
 #else
-int main(int argc, char** argv) {
+auto main(int argc, char** argv) -> int {
 #endif
-	std::string_view saveDir;
-	if (argc == 2) {
-		saveDir = argv[1];
-	}
+  std::string_view saveDir;
+  if (argc == 2) {
+    saveDir = argv[1];
+  }
 
-	gui::CreateContext();
-	glass::CreateContext();
+  gui::CreateContext();
+  glass::CreateContext();
 
-	gui::AddEarlyExecute([] { ImGui::DockSpaceOverViewport(ImGui::GetMainViewport()); });
-	gui::AddInit([] { ImGui::GetIO().ConfigDockingWithShift = true; });
+  gui::AddEarlyExecute(
+      [] { ImGui::DockSpaceOverViewport(ImGui::GetMainViewport()); });
+  gui::AddInit([] { ImGui::GetIO().ConfigDockingWithShift = true; });
 
-	glass::SetStorageName("imcan");
-	glass::SetStorageDir(saveDir.empty() ? gui::GetPlatformSaveFileDir() : saveDir);
+  glass::SetStorageName("imcan");
+  glass::SetStorageDir(saveDir.empty() ? gui::GetPlatformSaveFileDir()
+                                       : saveDir);
 
-	gui::AddInit([] { glass::ResetTime(); });
+  gui::AddInit([] { glass::ResetTime(); });
 
-	gui::AddLateExecute([] {gMainMenu.Display(); });
+  gui::AddLateExecute([] { gMainMenu.Display(); });
 
-	gMainMenu.AddMainMenu([] {
-		if (ImGui::BeginMenu("Windows")) {
-			bool dbcMgrVis = imcan::DbcGui::sDbcManager->GetWindow("Loader")->IsVisible();
-			std::string dbcMgrItem = dbcMgrVis ? "DBC Loader (Hide)" : "DBC Loader (Show)";
-			if (ImGui::MenuItem(dbcMgrItem.c_str())) {
-				imcan::DbcGui::sDbcManager->GetWindow("Loader")->SetVisible(!dbcMgrVis);
-			}
-			ImGui::EndMenu();
-		}
+  gMainMenu.AddMainMenu([] {
+    if (ImGui::BeginMenu("Windows")) {
+      bool dbcMgrVis =
+          imcan::DbcGui::sDbcManager->GetWindow("Loader")->IsVisible();
+      std::string dbcMgrItem =
+          dbcMgrVis ? "DBC Loader (Hide)" : "DBC Loader (Show)";
+      if (ImGui::MenuItem(dbcMgrItem.c_str())) {
+        imcan::DbcGui::sDbcManager->GetWindow("Loader")->SetVisible(!dbcMgrVis);
+      }
+      ImGui::EndMenu();
+    }
 
-		if (ImGui::BeginMenu("Info")) {
-			if (ImGui::MenuItem("About")) {
-				gAbout = true;
-			}
-			ImGui::EndMenu();
-		}
-	});
+    if (ImGui::BeginMenu("Info")) {
+      if (ImGui::MenuItem("About")) {
+        gAbout = true;
+      }
+      ImGui::EndMenu();
+    }
+  });
 
-	gui::AddLateExecute([] {
-		if (gAbout) {
-			ImGui::OpenPopup("About");
-			gAbout = false;
-		}
-		if (ImGui::BeginPopupModal("About")) {
-			ImGui::Text("ImCan: A modern CAN workstation");
-			ImGui::Separator();
-			ImGui::Text("Save location: %s", glass::GetStorageDir().c_str());
-			ImGui::Text("%.3f ms/frame (%.1f FPS)",
-									1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			if (ImGui::Button("Close")) {
-				ImGui::CloseCurrentPopup();
-			}
-			ImGui::EndPopup();
-		}
-	});
+  gui::AddLateExecute([] {
+    if (gAbout) {
+      ImGui::OpenPopup("About");
+      gAbout = false;
+    }
+    if (ImGui::BeginPopupModal("About")) {
+      ImGui::Text("ImCan: A modern CAN workstation");
+      ImGui::Separator();
+      ImGui::Text("Save location: %s", glass::GetStorageDir().c_str());
+      ImGui::Text("%.3f ms/frame (%.1f FPS)",
+                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      if (ImGui::Button("Close")) {
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
+    }
+  });
 
-	gui::Initialize("ImCan", 1024, 768, ImGuiConfigFlags_DockingEnable);
-	imcan::DbcGui::GlobalInit();
-	gui::Main();
+  gui::Initialize("ImCan", 1024, 768, ImGuiConfigFlags_DockingEnable);
+  imcan::DbcGui::GlobalInit();
+  gui::Main();
 
-	glass::DestroyContext();
-	gui::DestroyContext();
+  glass::DestroyContext();
+  gui::DestroyContext();
 
-	return 0;
+  return 0;
 }
